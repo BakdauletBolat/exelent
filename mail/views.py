c7188b8991742afa4b566cc1de7657777cefee5b
Your Name <you@example.com>
2021-04-16 11:13:38 +0600

    'a'

--- mail/views.py
+++ mail/views.py
@@ -15,14 +15,14 @@ def index(request):
             content = """
                Благодарим Вас за регистрацию! Ссылка на участие в семинаре будет направлена Вам на электронную почту в день проведения семинара.
             """
-            mail = send_mail(name, content, 'excellent@mail.kz', [cd['email'],'gulnaz_808@mail.ru','excellent.kaz@bk.ru','bakosh21345@gmail.com'])
-            if mail:
-                form.save()
-                messages.success(request, f'{name}! Благодарим Вас за регистрацию! Ссылка на участие в семинаре будет направлена Вам на электронную почту в день проведения семинара.')
-                return redirect('mailsend')
-            else:
-                messages.error(request, 'Ошибка при отправление письмо')
-                return redirect('mailsend')
+            # mail = send_mail(name, content, 'excellent@mail.kz', [cd['email'],'gulnaz_808@mail.ru','excellent.kaz@bk.ru','bakosh21345@gmail.com'])
+            #if mail:
+            form.save()
+            messages.success(request, f'{name}! Благодарим Вас за регистрацию! Ссылка на участие в семинаре будет направлена Вам на электронную почту в день проведения семинара.')
+            return redirect('mailsend')
+            # else:
+                #messages.error(request, 'Ошибка при отправление письмо')
+                #return redirect('mailsend')
         else:
             messages.error(request, 'Вы ввели неправильные данные')
             return redirect('mailsend')
