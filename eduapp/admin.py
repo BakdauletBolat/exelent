880d3e1123d377fab080e7c39aff8141074ab79c
bakdaulet <bakosh21345@gmail.com>
2021-07-01 14:49:48 +0600

    a

--- eduapp/admin.py
+++ eduapp/admin.py
@@ -5,6 +5,7 @@ from .models import Page, Slider
 # Apply summernote to all TextField in model.
 class PagesAdmin(SummernoteModelAdmin):  # instead of ModelAdmin
     summernote_fields = ('description', )
+    list_display = ('created_at', )
 
 admin.site.register(Page, PagesAdmin)
 
--- eduapp/models.py
+++ eduapp/models.py
@@ -16,14 +16,19 @@ class Slider(models.Model):
 
 class Page(models.Model):
     title = models.CharField(max_length=255)
+    created_at = models.DateTimeField()
     shortDescription = models.TextField()
     image = models.ImageField(upload_to="PageImages/")
-    file = models.FileField(upload_to='PdfFile/')
+    file = models.FileField(upload_to='PdfFile/', null=True, blank=True)
     description = models.TextField()
-    created_at = models.DateTimeField(auto_now=True)
+
+
 
     def get_absolute_url(self):
         return reverse('page_detail', args=[self.id])
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        ordering = ['-created_at']
