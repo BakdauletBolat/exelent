25d075cb4afe48cf7b86325386d522b697cfa1f1
Bob <bakdauletbolat@MacBook-Air-Bakdaulet.local>
2022-04-20 00:52:51 +0600

    a

--- exeleducenter/settings.py
+++ exeleducenter/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '2bfilmi^rxbihkryf&7mdjzni=k+!@01*az&0jk-#qu70z1v34'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 if DEBUG:
     ALLOWED_HOSTS = ['*']
